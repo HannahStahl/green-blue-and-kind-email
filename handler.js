8d4e018523cd796d4fe2dd54ad86c2d3c45a075e
Hannah Stahl <hls62@cornell.edu>
2020-02-11 20:43:55 -0600

    Converted email body to html and added cart items

--- handler.js
+++ handler.js
@@ -28,24 +28,43 @@ function generateError (code, err) {
 }
 
 function generateEmailParams (body) {
-  const { email, name, message } = JSON.parse(body)
-  if (!(email && name && message)) {
-    throw new Error('Missing parameters! Make sure to add parameters \'email\', \'name\', \'message\'.')
+  const { email, name, cart, message } = JSON.parse(body)
+  if (!(email && name && cart && message)) {
+    throw new Error('Missing parameters! Make sure to add parameters \'email\', \'name\', \'cart\', and \'message\'.')
   }
+  let subject = 'New message from greenblueandkind.com';
+  let html = '<html><body>';
+  if (cart.items) {
+    subject = 'New order request from greenblueandkind.com';
+    html += `<h1><b>${name}</b> has submitted a request for the following items:</h1>`;
+    html += `<table><thead><tr><td>Product</td><td>Size</td><td>Color</td><td>Quantity</td><td>Price</td></tr></thead><tbody>`;
+    cart.items.forEach((item) => {
+      html += `<tr><td>${item.name}</td><td>${item.size}</td><td>${item.color}</td><td>${item.quantity}</td><td>${item.price}</td></tr>`;
+    });
+    html += `</tbody></table><p><b>Estimated Total:</b> $${cart.total}</p>`;
+    if (message.length > 0) {
+      html += `<h2>Additional notes from ${name}</h2><p>"${message}"</p>`;
+    }
+    html += `<p><i>You can respond to this request by replying directly to this email.</i></p>`;
+  } else {
+    html += `<h1><b>${name}</b> has sent you a message:</h1><p>"${message}"</p>`;
+    html += `<p><i>You can respond to this message by replying directly to this email.</i></p>`;
+  }
+  html += `</body></html>`;
   return {
     Source: myEmail,
     Destination: { ToAddresses: [myEmail] },
     ReplyToAddresses: [email],
     Message: {
       Body: {
-        Text: {
+        Html: {
           Charset: 'UTF-8',
-          Data: `${name} says:\n\n"${message}"\n\nYou can reply directly to this email.`
+          Data: html,
         }
       },
       Subject: {
         Charset: 'UTF-8',
-        Data: 'New order request from greenblueandkind.com'
+        Data: subject,
       }
     }
   }
