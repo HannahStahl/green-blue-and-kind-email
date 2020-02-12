05a7d0c4a26811b78afc902adb336b8bc4785630
Hannah Stahl <hls62@cornell.edu>
2020-02-11 20:56:05 -0600

    Adjusted styling of email

--- handler.js
+++ handler.js
@@ -33,22 +33,36 @@ function generateEmailParams (body) {
     throw new Error('Missing parameters! Make sure to add parameters \'email\', \'name\', \'cart\', and \'message\'.')
   }
   let subject = 'New message from greenblueandkind.com';
-  let html = '<html><body>';
+  let html = `
+    <html><head><style>
+      p {
+        font-size: 16px;
+      }
+      td {
+        padding: 10px;
+        border: solid 1px gray;
+      }
+    </style></head><body>
+  `;
   if (cart.items) {
     subject = 'New order request from greenblueandkind.com';
-    html += `<h1><b>${name}</b> has submitted a request for the following items:</h1>`;
-    html += `<table><thead><tr><td>Product</td><td>Size</td><td>Color</td><td>Quantity</td><td>Price</td></tr></thead><tbody>`;
+    html += `
+      <h2><b>${name}</b> has submitted a request for the following items:</h2>
+      <table><thead><th><td>Product</td><td>Size</td><td>Color</td><td>Quantity</td><td>Price</td></th></thead><tbody>
+    `;
     cart.items.forEach((item) => {
-      html += `<tr><td>${item.name}</td><td>${item.size}</td><td>${item.color}</td><td>${item.quantity}</td><td>${item.price}</td></tr>`;
+      html += `<tr><td>${item.name}</td><td>${item.size}</td><td>${item.color}</td><td>${item.quantity}</td><td>$${item.price}</td></tr>`;
     });
     html += `</tbody></table><p><b>Estimated Total:</b> $${cart.total}</p>`;
     if (message.length > 0) {
-      html += `<h2>Additional notes from ${name}</h2><p>"${message}"</p>`;
+      html += `<h3>Additional notes from ${name}:</h3><p>"${message}"</p>`;
     }
     html += `<p><i>You can respond to this request by replying directly to this email.</i></p>`;
   } else {
-    html += `<h1><b>${name}</b> has sent you a message:</h1><p>"${message}"</p>`;
-    html += `<p><i>You can respond to this message by replying directly to this email.</i></p>`;
+    html += `
+      <h2><b>${name}</b> has sent you a message:</h2><p>"${message}"</p>
+      <p><i>You can respond to this message by replying directly to this email.</i></p>
+    `;
   }
   html += `</body></html>`;
   return {
